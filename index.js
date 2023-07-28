adfeda8bc42b7ee9cc36e0db3ecdb67b106ca631
karthickamypo <138156741+karthickamypo@users.noreply.github.com>
2023-07-28 12:46:08 +0530

    1 file modified

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>sudhar is ____</h1>`;
+appDiv.innerHTML = `<h1>qqqweqwwwwwwww</h1>`;
